d810ee15238efa362b38e0dcb744fc9486f3f64c
mateosannchez <mateosanchezvivas55@gmail.com>
2023-08-30 17:59:58 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import { store } from './store/Store.js';
+//import Store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
